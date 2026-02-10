b555f34d957a82b5ed0b91cc0d6d0d6de3c18e92
kevin <kevintate2@my.unt.edu>
2026-02-09 23:01:04 -0600

    updated keygen with conditional andfunctions for getting valid and expired key pairs

--- keygen.js
+++ keygen.js
@@ -1,8 +1,42 @@
 const crypto = require("crypto");
 let keys = [];
-
+//Function to generate key pair
 function generateKeyPair(expired = false){
     const { publicKey, privateKey } = crypto.generateKeyPairSync('rsa',{
         modulusLength: 2048
-    })
+    });
+
+const kid = crypto.randomBytes(16).toString('hex');
+//Expiration logic: set time back one hour to represent expired key
+let expiry;
+
+if (expired){
+    expiry = Date.now() - 3600000;
+} else {
+    expiry = Date.now() + 3600000;
 }
+
+return keypair = {
+    kid,
+    publicKey,
+    privateKey,
+    expiry
+};
+}
+//Create an expired key pair and a non-expired RSA key pair
+generateKeyPair(false);
+generateKeyPair(true);
+
+function getValid() {
+    const now = Date.now();
+    return keys.filter(key => key.expiry > now);
+}
+function getExpired(){
+    const now = Date.now();
+    return keys.filter(key => key.expiry < now);
+}
+
+module.exports = {
+    getValid,
+    getExpired,
+};
